4f47ff30d159a79d19611e09b0a685080bf340ee
WangYC <frederickwang99@gmail.com>
2024-03-08 13:24:31 +0800

    graoh visualization

--- api_graph.py
+++ api_graph.py
@@ -1,6 +1,9 @@
 from collections import deque
 import jsonlines
 from collections import defaultdict
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
 
 def information_collection(source):
     info_dict_list = []
@@ -71,11 +74,50 @@ def sample_paths(graph, start, n):
                 queue.append((neighbor, path + [neighbor]))
     return paths
 
+def showGraph(graph):
+# 字典形式的单向边
+    edges = graph
+    # # 创建图形对象
+    # G = nx.DiGraph()
+    # # 添加节点和边
+    # for node, neighbors in edges.items():
+    #     G.add_node(node)
+    #     for neighbor in neighbors:
+    #         G.add_edge(node, neighbor)
+    # # 绘制图形
+    # plt.figure(figsize=(12, 8))
+    # pos = nx.spring_layout(G)  # 使用Spring布局
+    # nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=5000, edge_color='gray', linewidths=2, font_size=16)
+    # plt.title("单向边关系图", fontsize=20)
+    # plt.show()
+    # # plt.savefig('api-graph.png', format='png', dpi=300)
+
+
+    nt = Network("500px", "1000px")
+    # 获取所有独特的节点
+    all_nodes = set()
+    for node, neighbors in edges.items():
+        all_nodes.add(node)
+        all_nodes.update(neighbors)
+    # 添加所有节点
+    for node in all_nodes:
+        nt.add_node(node)
+    # 再添加边
+    for node, neighbors in edges.items():
+        for neighbor in neighbors:
+            nt.add_edge(node, neighbor)
+    # 保存图形到本地HTML文件
+    nt.save_graph("graph.html")
+
 if __name__ == '__main__':
     info_dict_list = information_collection(source = "file")
     # print('info_dict_list: {}'.format(info_dict_list))
     graph = dict(build_graph(info_dict_list=info_dict_list))
-    # print(graph)
+    print(graph)
+
+    showGraph(graph)
+
+    print('------------------------------------------------')
 
     # # 示例图（使用字典表示）图中的键是节点，值是与每个节点直接连接的节点列表
     # example_graph = {
