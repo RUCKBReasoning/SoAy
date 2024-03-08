c0fe2d4bfece16056f89916820b5fbc47ef39ace
WangYC <frederickwang99@gmail.com>
2024-03-08 09:46:08 +0800

    api graph automation building and construction

--- api_graph.py
+++ api_graph.py
@@ -1,4 +1,57 @@
 from collections import deque
+import jsonlines
+from collections import defaultdict
+
+def information_collection(source):
+    info_dict_list = []
+    if source == 'input':
+        while(True):
+            Function_name = input('function name: (e for ending, s for skip)\n')
+            if Function_name == 'e':
+                break
+            elif Function_name == 's':
+                continue
+            else:
+                parameters_list = []
+                while(True):
+                    p_name = input('parameter name: (e for ending, s for skip)\n')
+                    if p_name == 'e':
+                        break
+                    elif p_name == 's':
+                        continue
+                    else:
+                        parameters_list.append(p_name)
+                return_list = []
+                while(True):
+                    r_name = input('return info name: (e for ending, s for skip)\n')
+                    if r_name == 'e':
+                        break
+                    elif r_name == 's':
+                        continue
+                    else:
+                        return_list.append(r_name)
+                info_dict_list.append({'function_name' : Function_name, 'parameters' : parameters_list, 'returns' : return_list})
+        # print('info_dict_list: {}'.format(info_dict_list))
+
+    elif source == 'file':
+        file_name = 'config/function_config.jsonl'
+        with jsonlines.open(file_name, 'r') as f:
+            for line in f:
+                info_dict_list.append(line)
+        # print('info_dict_list: {}'.format(info_dict_list))
+        
+    return info_dict_list
+
+def build_graph(info_dict_list):
+    graph = defaultdict(list)
+    # Populate the graph
+    for func in info_dict_list:
+        for other_func in info_dict_list:
+            # if func['function_name'] != other_func['function_name']:
+                # Check if any return value of func is a parameter of other_func
+            if any(ret_val in other_func['parameters'] for ret_val in func['return']):
+                graph[func['function_name']].append(other_func['function_name'])
+    return graph
 
 def sample_paths(graph, start, n):
     # 初始化队列和结果列表
@@ -19,25 +72,31 @@ def sample_paths(graph, start, n):
     return paths
 
 if __name__ == '__main__':
-    # 示例图（使用字典表示）
-    # 图中的键是节点，值是与每个节点直接连接的节点列表
-    example_graph = {
-        '1': ['3', '4', '5', '6', '7'],
-        '2': ['5'],
-        '3': ['1', '3', '4'],
-        '4': [],
-        '5': ['1', '4', '6'],
-        '6': [],
-        '7': ['5']
-    }
-    # 给定起始节点
-    start_node_list = ['1', '2']
+    info_dict_list = information_collection(source = "file")
+    # print('info_dict_list: {}'.format(info_dict_list))
+    graph = dict(build_graph(info_dict_list=info_dict_list))
+    # print(graph)
+
+    # # 示例图（使用字典表示）图中的键是节点，值是与每个节点直接连接的节点列表
+    # example_graph = {
+    #     '1': ['3', '4', '5', '6', '7'],
+    #     '2': ['5'],
+    #     '3': ['1', '3', '4'],
+    #     '4': [],
+    #     '5': ['1', '4', '6'],
+    #     '6': [],
+    #     '7': ['5']
+    # }
+
+    # # 给定起始节点
+    start_node_list = ['searchPerson', 'searchPublication']
     # 给定跳数
     max_hops = 3
 
     for start_node in start_node_list:
-        sampled_paths = sample_paths(example_graph, start_node, max_hops)
+        sampled_paths = sample_paths(graph, start_node, max_hops)
         # 打印结果
         # print(f"所有{max_hops}跳以内的路径：")
         for path in sampled_paths:
-            print(path)
+            print(' -> '.join(path))
+    
